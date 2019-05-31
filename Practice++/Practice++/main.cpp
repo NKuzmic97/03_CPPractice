0dafed7d8d4a89c3248e0fc05b2674ccf066c1cc
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-05-31 13:26:57 +0200

    Const keyword

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -2,9 +2,39 @@
 #include <string>
 #include <stdlib.h>
 
+class Entity {
+private:
+	int m_X, m_Y;
+	mutable int variable;
+public:
+	int GetX() const{
+		variable = 1;
+		return m_X;
+	}
+
+	int GetX() {
+		return m_X;
+	}
+
+	void SetX(int x) {
+		m_X = x;
+	}
+};
+
+void PrintEntity(const Entity& e) {
+
+	std::cout << e.GetX() << std::endl;
+}
+
 int main() {
-	char name[] = "Cherno";
-	name[2] = 'a';
-	std::cout << name << std::endl;
+
+	Entity* e;
+	PrintEntity(*e);
+
+	const int MAX_AGE = 90;
+	const int* const a = new int;
+	
+	std::cout << *a << std::endl;
+
 	std::cin.get();
 }
