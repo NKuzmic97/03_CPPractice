519924162e9e95dd3dcc0a17f1374e221cb42118
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-05-31 03:08:19 +0200

    Const char*

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <string>
-
-void PrintString(const std::string& string) {
-	std::cout << string << std::endl;
-}
+#include <stdlib.h>
 
 int main() {
-	std::string name = std::string ("Cherno") + " hello!";
-	bool contains = name.find("no") != std::string::npos;
+	char name[] = "Cherno";
+	name[2] = 'a';
 	std::cout << name << std::endl;
 	std::cin.get();
 }
