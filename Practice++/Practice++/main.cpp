e8a9a0c395a406f09baed4bb3043571b58772fd8
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-06-01 20:38:08 +0200

    Stack & heap initalization

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <string>
 
-static int s_Level = 6;
-static int s_Speed = 2;
+using String = std::string;
+
+class Entity {
+private:
+	String m_Name;
+public:
+	Entity() :m_Name("Unknown") { std::cout << "0"; }
+	Entity(const String& name):m_Name(name){
+		std::cout << "1";
+	}
+	const String& GetName() const { return m_Name; }
+};
 
 
 int main() {
-	if (s_Level > 5)
-		s_Speed = 10;
-	else
-		s_Speed = 5;
-
-	s_Speed = s_Level > 5 && s_Level < 100 ? s_Level > 10 ? 15 : 10 : 5;
-
-	std::cout << s_Speed << std::endl;
-
+	Entity* eptr;
+	{
+		Entity* entity = new Entity("Cherno");
+		eptr = entity;
+		std::cout << eptr->GetName() << std::endl;
+	}
 	std::cin.get();
+	delete eptr;
 }
