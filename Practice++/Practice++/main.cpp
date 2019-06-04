491d5a13aee73941a021786501d20594e74036a4
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-06-04 23:11:32 +0200

    explicit implicit

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include <array>
+#include <string>
+
 class Entity {
-	static const int size=5;
-	int example[size];
-	std::array<int, 5> another;
+	std::string m_Name;
+	int m_Age;
 public:
-	Entity()
-	{
-		for (int i = 0; i < another.size; i++)
-		example[i] = 2;
-	}
+	 Entity(const std::string& name):m_Name(name){}
+	 Entity(int age):m_Name("Unknown"),m_Age(age){}
 };
 
+void PrintEntity(const Entity& entity) {
+
+}
+
 int main() {
-	Entity e;
+	PrintEntity(22);
+	PrintEntity(std::string("Cherno"));
+	Entity a = "Cherno";
+	Entity b =(Entity) 22;
 	std::cin.get();
 }
