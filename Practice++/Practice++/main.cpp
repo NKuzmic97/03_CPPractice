d10f1787e25df0cf80cf277ba6819d035271a250
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-06-04 23:19:20 +0200

    explicit implicit

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 #include <string>
-
-using String = std::string;
-
 class Entity {
 private:
-	String m_Name;
+	std::string m_Name;
+	int m_Age;
 public:
-	Entity() :m_Name("Unknown") { std::cout << "0"; }
-	Entity(const String& name):m_Name(name){
-		std::cout << "1";
-	}
-	const String& GetName() const { return m_Name; }
+	explicit Entity(const std::string& name):m_Name(name),m_Age(-1){}
+	explicit Entity(int age):m_Name("Unknown"),m_Age(age){}
 };
-
-
+void PrintEntity(const Entity& entity) {
+	//Printing
+}
 int main() {
-	int a = 2;
-	int* b = new int[50];
+	PrintEntity(22);
+	PrintEntity(Entity("Cherno"));
 
-	Entity* entity = new(b) Entity();
-	delete entity;
-	delete[] b;
+	Entity a = "Cherno";
+	Entity b = 22;
 	std::cin.get();
 }
