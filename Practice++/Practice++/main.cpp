db0d53ff60c1b1777a3375c0208bab2dccad52ca
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-06-06 01:07:05 +0200

    copy constructor

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,27 +1,49 @@
 #include <iostream>
 #include <string>
-#include <memory>
-class Entity {
+
+class String {
+private:
+	char* m_Buffer;
+	unsigned int m_Size;
 public:
-	Entity() {
-		std::cout << "Created Entity!" << std::endl;
+	String(const char* string) {
+		m_Size = strlen(string);
+		m_Buffer = new char[m_Size+1];
+		memcpy(m_Buffer, string, m_Size);
+		m_Buffer[m_Size] = 0;
 	}
-	~Entity() {
-		std::cout << "Destroyed Entity!" << std::endl;
+	~String() {
+		delete[] m_Buffer;
 	}
-	void Print(){}
+	char& operator[](unsigned int index) {
+		return m_Buffer[index];
+	}
+	//String(const String& other) {
+		//memcpy(this, &other, sizeof(String));
+	String(const String& other):m_Size(other.m_Size) {
+		std::cout << "Copy construcotr" << std::endl;
+		m_Buffer = new char[m_Size + 1];
+		memcpy(m_Buffer, other.m_Buffer, m_Size+1);
+	}
+	friend std::ostream& operator<<(std::ostream& stream, const String& string);
 };
 
+std::ostream& operator<<(std::ostream& stream, const String& string) {
+	stream << string.m_Buffer;
+	return stream;
+}
+
+void PrintString(const String& string) {
+	String copy = string;
+	std::cout << copy << std::endl;
+}
 
 int main() {
-
-	std::weak_ptr<Entity> e0;
-	{
-		std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
-		std::weak_ptr<Entity> weakPtr = sharedEntity;
-		e0 = sharedEntity;
-	}
-
-
+	String string = "Cherno";
+	std::cout << string << std::endl;
+	String second = string;
+	second[2] = 'a';
+	PrintString(string);
+	PrintString(second);
 	std::cin.get();
 }
