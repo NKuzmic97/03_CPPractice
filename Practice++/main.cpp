a4420b40a535db4703672316bf89491cdab2d548
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-09-03 16:04:48 +0200

    Binary search tree practice.

--- Practice++/main.cpp
+++ Practice++/main.cpp
@@ -1,117 +1,116 @@
 #include <iostream>
 #include <vector>
 #include "Practice++/Common.h"
+#include <assert.h>
 
-// Swaps two values in memory
-// Takes locations "a" and "b" as arguments
+struct Node {
+	// Pointers to left and right nodes
+	Node* left;
+	Node* right;
 
-void swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+	// Data
+	int m_data;
 
-// Initialize a vector with random numbers
-void init_vector(vector<int> &vec, const unsigned n) {
-	for (unsigned i = 0; i < n; i++) vec.push_back(rand() % 100);
-}
-
-// Helper function for printing a vector
-void print_vector(const vector<int> &vec) {
-	for (unsigned i = 0; i < vec.size(); i++) {
-		cout << vec[i] << "\t";
+	// Constructor
+	Node(int data) {
+		m_data = data;
+		left = nullptr;
+		right = nullptr;
 	}
-	cout << endl;
+};
+
+class BinarySearchTree {
+public:
+	// Root node of the tree
+	Node* root;
+	
+	// Simple constructor sets the root to NULL
+	BinarySearchTree() { root = nullptr; }
+
+	// Simple method for inserting a node
+	Node* insert_node(Node* node, int data);
+
+	// Search for a node with m_data "data"
+	Node* search(int data);
+};
+
+// Resursively traverse the tree until the node is nullptr
+// (insertion point)
+Node* BinarySearchTree::insert_node(Node* node, int data) {
+	// Case that we are inserting the node here
+	if (node == nullptr) {
+		std::cout << "X" << std::endl;
+		return new Node(data);
+	}
+	else if (data < node->m_data) {
+		std::cout << "L";
+		node->left = insert_node(node->left, data);
+	}
+	else if (data > node->m_data) {
+		std::cout << "R";
+		node->right = insert_node(node->right, data);
+	}
+	// In case we have duplicate entries (illegal)
+	else {
+		assert(false);
+	}
+	// If this node was not nullptr, return it unchanged
+	return node;
 }
 
+Node* BinarySearchTree::search(int data) {
+	// Temporary node to use for traversal (init. to root)
+	Node* temp = root;
 
-// Helper function to print out sub-list with original vector
-void print_range(const vector<int> &vec, unsigned start, unsigned end) {
-	for (unsigned i = start; i < end + 1; i++) {
-		cout << vec[i] << endl;
+	// Keep traversing the tree until the node is found (maybe not)
+	while (temp != nullptr) {
+		// Return the node we are looking for
+		if (data == temp->m_data) {
+			std::cout << "X" << std::endl;
+			return temp;
+		}
+		// Go down to the left path
+		else if(data < temp->m_data){
+			std::cout << "L" << std::endl;
+			temp = temp->left;
+		}
+		// Go down to the right path
+		else {
+			std::cout << "R";
+			temp = temp->right;
+
+		}
 	}
-	cout << endl;
+	// We didn't find the node..
+	return nullptr;
 }
 
-// Helper function to print intermediate results
-void print_sub_vectors(const vector<int> &v1,const vector<int> &v2) {
-	for (unsigned i = 0; i < v1.size(); i++) {
-		cout << v1[i] << " ";
-	}
-	cout << "\t";
-	for (unsigned i = 0; i < v2.size(); i++) {
-		cout << v2[i] << " ";
-	}
-	cout << endl;
-}
-
-// Merges to sorted sub-lists
-void merge(vector<int> &vec, unsigned start, unsigned middle, unsigned end) {
-	cout << "Call merge for Start = " << start << ", End = " << end << endl;
-	// Not sorting in place. Use two temp vectors
-	// Initialize left-hand elements
-	unsigned elements_left = middle - start + 1;
-	vector<int> tmp_left;
-	for (unsigned i = 0; i < elements_left; i++) tmp_left.push_back(vec[start + i]);
-	// Initialize right-hand elements
-	unsigned elements_right = end - middle;
-	vector<int> tmp_right;
-	for (unsigned i = 0; i < elements_right; i++) tmp_right.push_back(vec[middle + 1 + i]);
-
-	print_sub_vectors(tmp_left, tmp_right);
-
-	// Compare vectors and add elements to the final result
-	unsigned i = 0;
-	unsigned j = 0;
-	int pos = start;
-	while ((i < tmp_left.size()) && (j < tmp_right.size())) {
-		// Add left-hand element if its smaller
-		// Otherwise add right-hand elements
-		if (tmp_left[i] <= tmp_right[j]) vec[pos] = tmp_left[i++];
-		else vec[pos] = tmp_right[j++];
-		pos++;
-	}
-	// Clean up remaining left-hand elements
-	while (i < tmp_left.size()) vec[pos++] = tmp_left[i++];
-	// Clean up remaining right-hand elements
-	while (j < tmp_right.size()) vec[pos++] = tmp_right[j++];
-
-	print_range(vec, start, end);
-}
-
-// Recursive function for sorting the array
-void merge_sort(vector<int> &vec, unsigned left, unsigned right) {
-	cout << "Merge sort for Left = " << left << " Right = " << right << endl;
-	// Merge until we are left with a single element
-	if (left < right) {
-		// Calculate mid-point of vector
-		int mid = (left + right) / 2;
-
-		// Sort the sub-vectors
-		merge_sort(vec, left, mid);
-		merge_sort(vec, mid + 1, right);
-
-		// Merge the two sub-lists
-		merge(vec, left, mid, right);
-	}
-}
 
 int main() {
-	// Vector that we will sort
-	vector<int> vec;
+	// Create a new binary search tree
+	BinarySearchTree bst;
 
-	// Initialize a vecotr with "n" elements
-	unsigned n = 7;
-	init_vector(vec, n);
+	// Set the random seed 
+	srand(12345);
 
-	// Print the vectors before sorting
-	cout << "Original vector " << endl;
-	print_vector(vec);
+	// Insert 10 random nodes
+	int data;
+	vector<int> data_vec;
+	for (int i = 0; i < 10; i++) {
+		// Random number between 0-999
+		data = rand() % 1000;
+		data_vec.push_back(data);
+		std::cout << "Inserting node with data " << data << std::endl;
+		
+		// Insert the node (save the root if it's the first insert)
+		bst.root = bst.insert_node(bst.root, data);
+	}
+	std::cout << std::endl;
 
-	merge_sort(vec, 0, vec.size() - 1);
-	// Print out he result
-	cout << "Final vector" << endl;
-	print_vector(vec);
-
+	// Search for each of the inserted nodes
+	for (int i : data_vec) {
+		std::cout << "Searching for node with data " << i << std::endl;
+		bst.search(i);
+	}
 	return 0;
 }
