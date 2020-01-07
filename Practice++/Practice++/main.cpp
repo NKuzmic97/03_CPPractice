a5f6978eff4c05663d7e1baba2e8694c97747085
Nemanja Kuzmić <kuzmicnemanja@live.com>
2020-01-07 01:56:58 +0100

    Exceptions - Tutorial 22.1

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,292 +1,108 @@
 #include <iostream>
 #include <string>
-#include <conio.h>
-#include <random>
-#include <algorithm>
-#include <memory>
+#include <vector>
 
-class Dice {
+class Cat {
 public:
-	int Roll(int nDice) {
-		int total = 0;
-		for (int n = 0; n < nDice; n++) {
-			total += d6(rng);
-		}
-		return total;
+	Cat() {
+		std::cout << "Constructing a Cat" << std::endl;
 	}
-private:
-	std::uniform_int_distribution<int> d6 = std::uniform_int_distribution<int>(1, 6);
-	std::mt19937 rng = std::mt19937(std::random_device{}());
+	Cat(Cat&&) noexcept {
+		// cats wont be moved but instead copied if move constructor is not marked as noexcept
+		std::cout << "Moving a Cat" << std::endl;
+	}
+
+	Cat(const Cat&) {
+		std::cout << "Copying a Cat" << std::endl;
+	}
 };
 
-struct Attributes {
-	int hp;
-	int speed;
-	int power;
-};
-
-class Weapon {
+class Annoucer {
 public:
-	Weapon(const std::string& name, int rank)
-		:
-		name(name),
-		rank(rank) {
-	}
-	const std::string& GetName() const {
-		return name;
-	}
-	int GetRank() const {
-		return rank;
-	}
-	virtual int CalculateDamage(const Attributes& attr, Dice& d) const = 0;
-	virtual ~Weapon() = default;
-private:
-	std::string name;
-	int rank;
-};
+	Annoucer(std::string name_)
+	:
+	name(name_)
 
-class MemeFighter {
-public:
-	const std::string& GetName() const {
-		return name;
-	}
-	bool IsAlive() const {
-		return attr.hp > 0;
-	}
-	int GetInitiative() const {
-		return attr.speed + Roll(2);
-	}
-	void Attack(MemeFighter& other) const {
-		if (IsAlive() && other.IsAlive()) {
-			std::cout << name << " attacks " << other.GetName() << " with his " << pWeapon->GetName()
-				<< "!" << std::endl;
-			ApplyDamageTo(other, pWeapon->CalculateDamage(attr, d));
-		}
-	}
-	virtual void Tick() {
-		if (IsAlive()) {
-			const int recovery = Roll();
-			std::cout << name << " recovers " << recovery << " HP." << std::endl;
-			attr.hp += recovery;
-		}
-	}
-	virtual void SpecialMove(MemeFighter&) = 0;
-	virtual ~MemeFighter() = default;
-	void GiveWeapon(std::unique_ptr<Weapon> pNewWeapon) {
-		pWeapon = std::move(pNewWeapon);
-	}
-	std::unique_ptr<Weapon> PilferWeapon() {
-		return std::move(pWeapon);
-	}
-	bool HasWeapon() const {
-		return pWeapon != nullptr;
-	}
-	const Weapon& GetWeapon() const {
-		return *pWeapon;
-	}
-protected:
-	MemeFighter(const std::string& name, int hp, int speed, int power, std::unique_ptr<Weapon> pWeapon)
-		:
-		name(name),
-		attr({ hp,speed,power }),
-		pWeapon(std::move(pWeapon))
 	{
-		std::cout << name << " enters the ring!" << std::endl;
+		std::cout << "Annoucer " << name << " created." << std::endl;
 	}
-	
-	void ApplyDamageTo(MemeFighter& target, int damage) const {
-		target.attr.hp -= damage;
-		std::cout << target.name << " takes " << damage << " damage." << std::endl;
-		if (!target.IsAlive()) {
-			std::cout << "As the life leaves " << target.name << "'s body, so does the poop." << std::endl;
-		}
+	~Annoucer() noexcept {
+		std::cout << "Annoucer " << name << " deleted." << std::endl;
+		
+		//throw std::runtime_error("This program is dead");
+		// never throw exceptions from destructors
 	}
-	int Roll(int nDice = 1) const {
-		return d.Roll(nDice);
-	}
-protected:
-	Attributes attr;
-	std::string name;
 private:
-	std::unique_ptr<Weapon> pWeapon;
-	mutable Dice d;
+	std::string name;
 };
 
-class Fists : public Weapon {
-public:
-	Fists()
-		:
-		Weapon("fists", 0) {
-	}
-	virtual int CalculateDamage(const Attributes& attr, Dice& d) const {
-		return attr.power + d.Roll(2);
-	}
-};
+int sum(int a,int b) {
+	Annoucer j("j");
+	Annoucer k("k");
 
-class Knife : public Weapon {
-public:
-	Knife()
-		:
-		Weapon("knife", 2) {
-	}
-	virtual int CalculateDamage(const Attributes& attr, Dice& d) const {
-		return attr.speed * 3 + d.Roll(3);
-	}
-};
 
-class Bat : public Weapon {
-public:
-	Bat()
-		:
-		Weapon("bat", 1) {
-	}
-	virtual int CalculateDamage(const Attributes& attr, Dice& d) const {
-		return attr.power * 2 + d.Roll(1);
-	}
-};
+	// this will get deleted by RAII when thrown exception
+	auto p = std::make_unique<Annoucer>("p");
 
-class MemeFrog : public MemeFighter {
-public:
-	MemeFrog(const std::string& name, std::unique_ptr<Weapon> pWeapon)
-		:
-		MemeFighter(name, 69, 7, 14, std::move(pWeapon)) {
+	// Line below leaks memory in case of exception thrown.
+	//auto p = new Annoucer("p");
+	
+	if(a==70) {
+		throw std::runtime_error("<a> is bad number!");
 	}
-	void SpecialMove(MemeFighter& other) override {
-		if (IsAlive() && other.IsAlive()) {
-			if (Roll() > 4) {
-				std::cout << GetName() << " attacks " << other.GetName() << " with a rainbow beam!" << std::endl;
-				ApplyDamageTo(other, Roll(3) + 20);
-			}
-			else {
-				std::cout << GetName() << " falls off his unicycle." << std::endl;
-			}
-		}
-	}
-	void Tick() override {
-		if (IsAlive()) {
-			std::cout << GetName() << " is hurt by the bad AIDS!" << std::endl;
-			ApplyDamageTo(*this, Roll());
-			MemeFighter::Tick();
-		}
-	}
-	~MemeFrog() {
-		std::cout << "Destroying MemeFrog '" << name << "'!" << std::endl;
-	}
-};
 
-class MemeStoner : public MemeFighter {
-public:
-	MemeStoner(const std::string& name, std::unique_ptr<Weapon> pWeapon)
-		:
-		MemeFighter(name, 80, 4, 10, std::move(pWeapon)) {
+	if(b==450) {
+		throw std::logic_error("<b> is bad number!");
 	}
-	void SpecialMove(MemeFighter&) override {
-		if (IsAlive()) {
-			if (Roll() > 3) {
-				std::cout << GetName() << " smokes the dank sticky icky, becoming " << "Super " << GetName() << std::endl;
-				name = "Super " + name;
-				attr.speed += 3;
-				attr.power = (attr.power * 69) / 42;
-				attr.hp += 10;
-			}
-			else {
-				std::cout << GetName() << " spaces out." << std::endl;
-			}
-		}
-	}
-	~MemeStoner() {
-		std::cout << "Destroying MemeStoner '" << name << "'!" << std::endl;
-	}
-};
 
-void TakeWeaponIfDead(MemeFighter& taker, MemeFighter& giver) {
-	if (taker.IsAlive() && !giver.IsAlive() && giver.HasWeapon()) {
-		if (giver.GetWeapon().GetRank() > taker.GetWeapon().GetRank()) {
-			std::cout << taker.GetName() << " takes the " << giver.GetWeapon().GetName()
-				<< " from " << giver.GetName() << "'s still-cooling corpse." << std::endl;
-			taker.GiveWeapon(giver.PilferWeapon());
-		}
-	}
+	Annoucer c("c");
+	
+	return a + b;
 }
 
-void Engage(MemeFighter& f1, MemeFighter& f2) {
-	// pointers for sorting purposes
-	auto* p1 = &f1;
-	auto* p2 = &f2;
-	// determine attack order
-	if (p1->GetInitiative() < p2->GetInitiative()) {
-		std::swap(p1, p2);
+int f(int x,int y,int z) {
+	try {
+		return sum(x, y) * sum(y, z);
 	}
-	// execute attacks
-	p1->Attack(*p2);
-	TakeWeaponIfDead(*p1, *p2);
-	p2->Attack(*p1);
-	TakeWeaponIfDead(*p2, *p1);
-}
-
-void DoSpecials(MemeFighter& f1, MemeFighter& f2) {
-	// pointers for sorting purposes
-	auto* p1 = &f1;
-	auto* p2 = &f2;
-	// determine attack order
-	if (p1->GetInitiative() < p2->GetInitiative()) {
-		std::swap(p1, p2);
+	catch (const std::runtime_error& err) {
+		std::cout << "Caught a std::runtime_error in f: " << err.what() << std::endl;
+		throw;
 	}
-	// execute attacks
-	p1->SpecialMove(*p2);
-	TakeWeaponIfDead(*p1, *p2);
-	p2->SpecialMove(*p1);
-	TakeWeaponIfDead(*p2, *p1);
 }
 
 int main() {
-	std::vector<std::unique_ptr<MemeFighter>> t1;
-	t1.push_back(std::make_unique<MemeFrog>("Dat Boi", std::make_unique<Fists>()));
-	t1.push_back(std::make_unique<MemeStoner>("Good Guy Greg",std::make_unique<Bat>()));
-	t1.push_back(std::make_unique<MemeFrog>("the WB Frog", std::make_unique<Knife>()));
-
-	std::vector<std::unique_ptr<MemeFighter>> t2;
-	t2.push_back(std::make_unique<MemeStoner>("Chong", std::make_unique<Fists>()));
-	t2.push_back(std::make_unique<MemeStoner>("Scumbag Steve", std::make_unique<Bat>()));
-	t2.push_back(std::make_unique<MemeFrog>("Pepe", std::make_unique<Knife>()));
-
-	const auto alive_pred = [](const std::unique_ptr<MemeFighter>& pf) { return pf->IsAlive(); };
-	while (
-		std::any_of(t1.begin(), t1.end(), alive_pred) &&
-		std::any_of(t2.begin(), t2.end(), alive_pred)) 
 	
-	{
-		std::random_shuffle(t1.begin(), t1.end());
-		std::partition(t1.begin(), t1.end(), alive_pred);
-		std::random_shuffle(t2.begin(), t2.end());
-		std::partition(t2.begin(), t2.end(), alive_pred);
-
-		for (size_t i = 0; i < t1.size(); i++) {
-			Engage(*t1[i], *t2[i]);
-			DoSpecials(*t1[i], *t2[i]);
-			std::cout << "------------------------------------" << std::endl;
-		}
-		std::cout << "=====================================" << std::endl;
-
-		for (size_t i = 0; i < t1.size(); i++) {
-			t1[i]->Tick();
-			t2[i]->Tick();
-		}
-		std::cout << "=====================================" << std::endl;
-
-		std::cout << "Press any key to continue...";
-		while (!_kbhit());
-		_getch();
-		std::cout << std::endl << std::endl;
+	try {
+		std::cout << f(70,75,450) << std::endl;
+	}
+	catch(const std::runtime_error& err) {
+		std::cout << "Caught a std::runtime_error in main: " << err.what() << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cout << "Caught an std::exception in main: " << e.what() << std::endl;
+	}
+	catch(...) {
+		std::cout << "Caught *something* in main. " << std::endl;
 	}
 
-	if (std::any_of(t1.begin(), t1.end(), alive_pred)) {
-		std::cout << "Team ONE is victorious!" << std::endl;
-	}
-	else {
-		std::cout << "Team TWO is victorious!" << std::endl;
-	}
+	std::cout << "-------------------------------------------"<< std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
 
-	while (!_kbhit());
+	
+	std::vector<Cat> cats;
+	
+	std::cout << std::endl;
+	cats.emplace_back();
+	
+	std::cout << std::endl;
+	cats.emplace_back();
+	
+	std::cout << std::endl;
+	cats.emplace_back();
+	
+	std::cout << std::endl;
+
+	std::cin.get();
 	return 0;
-}
+} 
