3a68138cf8b863cf6586901496597862e49ecae7
Nemanja Kuzmić <kuzmicnemanja@live.com>
2020-01-11 01:43:25 +0100

    Tutorial 24.2 - Usage of hash table (unordered map)

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,62 +1,39 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
-#include <map>
-#include <algorithm>
-#include <vector>
-#include <set>
+#include <unordered_map>
 
 int main() {
-	/* USAGE OF STD::MAP */
-	
-	/* std::string response;
-	std::string command;
+	// create uo map and init
 
-	std::map<std::string, std::string> map;
-
-	//init
-
-	map.emplace("aaaaa", "bbbb");
-	map.insert({ "crabdance", "crab people, crab people!" });
-	map.insert({ "cashme","owsigh, hawbadah?" });
-	map.insert({ "so", "some dumb text bla bla" });
-
-	// remove_if doesn't work with associative containers
-	//std::remove_if(map.begin(), map.end(), [](auto& el) { return el.first.size() > 2; });
-
-
-	for(auto i=map.begin();i!=map.end();) {
-		if(i->first.size() > 2) {
-			i = map.erase(i);
+	std::unordered_map<std::string, int> map({
+		
+		{ "one",1 },
+		{ "two",2 },
+		{ "three",3 },
+		{ "four",4 },
+		{ "five",5 },
+		{ "six",6 },
+		{ "seven",7 },
+		{"eight",8},
+		{"nine",9},
+		{"zero",0}
+		
 		}
-		else {
-			++i;
-		}
+	);
+
+	std::string input;
+	std::cin >> input;
+
+	auto i = map.find(input);
+
+	// print val if key exists
+	if(i!= map.end()) {
+		std::cout << i->second << std::endl;
 	}
-
-	for(auto& key : map) {
-		std::cout << key.first << std::endl;
-		//key.first = "100"; // unallowed
+	else {
+		std::cout << "Not found" << std::endl;
 	}
-	
-	std::cin >> command;
-
-	std::cout << map[command] << std::endl;
-
-	*/
-
-	
-	std::vector<int> input = { 1,3,1,1,3,5,0,3,9,6,4,4,7,6,4,5,6,1,0 };
-	std::set<int> seen;
-
-	for(int in : input) {
-		auto result = seen.insert(in);
-		if(result.second) {
-			std::cout << in << " ";
-		}
-	}
-
-	std::cout << std::endl;
 
 	while (!_kbhit());
 	return 0;
