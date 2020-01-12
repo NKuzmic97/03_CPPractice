4ca0444161398b2efc7d0323c92b9b51a9e3fd6c
Nemanja Kuzmić <kuzmicnemanja@live.com>
2020-01-12 01:08:13 +0100

    Tutorial 25 - Basic string switch.

--- Practice++/Practice++/main.cpp
+++ Practice++/Practice++/main.cpp
@@ -1,47 +1,49 @@
 #include <conio.h>
 #include <memory>
 #include <iostream>
+#include <string>
 #include <unordered_map>
 #include <functional>
 
-void Fifty(){
-	std::cout << "50" << std::endl;
+class StringSwitch {
+public:
+	std::function<void()>& Case(const std::string& str) {
+		return map[str];
+	}
+
+	std::function<void()>& Default() {
+		return def;
+	}
+
+	void operator[](const std::string& str) const {
+		auto i = map.find(str);
+		if(i!= map.end()) {
+			i->second();
+		}
+		else {
+			def();
+		}
+	}
+	
+private:
+	std::unordered_map<std::string, std::function<void()>> map;
+	std::function<void()> def = []() {};
 };
 
-void Fortytwo() {
-	std::cout << "42" << std::endl;
-};
-
-int DoubleNum(int x) {
-	return x * 2;
-}
-
-int TripleNum(int x) {
-	return x * 3;
-}
 
 int main() {
-	 std::unordered_map<std::string, std::function<void()>> sw;
 
-	 sw["fifty"] = []()
-	 {
-		 std::cout << "50" << std::endl;
-	 };
+	int x = 70;
 	
-	 sw["forty-two"] = Fortytwo;
+	StringSwitch sw;
+	sw.Case("foo") = [&]() { std::cout << "you did a foo " << x << std::endl; };
+	sw.Case("bar") = []() { std::cout << "you did a bar" << std::endl; };
 
-	 sw["forty-two"]();
+	sw.Default() = []() {std::cout << "error" << std::endl; };
 
-	/* int(*pFunc)(int);
-
-	pFunc = DoubleNum;
-
-	std::cout << pFunc(3) << std::endl;
-
-	pFunc = TripleNum;
-	std::cout << pFunc(3) << std::endl;
-	*/
-
+	sw["foo"];
+	sw["nemanja"];
+	
 	while (!_kbhit());
 	return 0;
 }
